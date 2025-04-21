6cc008f55875d9b5adba8a5b9c65bae549137122
mansimodi278 <mansimodinyc@gmail.com>
2025-04-20 23:28:30 -0400

    worked on coding the movement settings

--- scripts/red_panda.gd
+++ scripts/red_panda.gd
@@ -1,25 +1,36 @@
 extends CharacterBody2D
 
+@export var speed = 300
+@export var gravity = 30
+@export var jump_force = 300
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+onready var animation_player = $AnimationPlayer
 
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+func _physics_process(delta):
+	if !is_on_floor():
+		velocity.y += gravity
+		if velocity.y > 1000:
+			velocity.y = 1000
+			
+	if Input.is_action_just_pressed("jump") && is_on_floor():
+		velocity.y = -jump_force
+		
+	var animation = "tail"
+	if abs(input_vector.x) > 0:
+		animation = "jump"
+	if velocity.y < 0 && !is_on_floor():
+		animation = "jump"
+		
+		if velocity.y > 0 && !is_on_floor():
+		animation = "fall"
+		
+		animation_player.play(animation)
+		
+		
+	var horizontal_direction = Input.get_axis("move_left ", "move_right")
+	
+	velocity.x = speed * horizontal_direction
+	
 	move_and_slide()
+	
+	print(velocity)
