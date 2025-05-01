2f23156bb28bf3d03467416d2b10b590857d206a
mansimodi278 <mansimodinyc@gmail.com>
2025-05-01 13:43:24 -0400

    animations now work!

---
+++ new red_panda.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+
+var jumping = false
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -900.0
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		$AnimatedSprite2D.play("jump")
+		jumping = true
+	
+	elif not is_on_floor() and not jumping: 
+		$AnimatedSprite2D.play("fall")
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if not is_on_floor():
+			$AnimatedSprite2D.play("tail")
+
+
+	move_and_slide()
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if $AnimatedSprite2D.animation == "jump":
+		jumping = false
